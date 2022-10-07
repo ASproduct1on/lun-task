98046b2fcee9867474ff0b89f5e63e598509ebe1
Andrii Skega <Game.a.s.production@gmail.com>
2022-10-07 20:26:34 +0200

    fix: fix visual bugs

--- src/components/summary-screen/summary-screen.js
+++ src/components/summary-screen/summary-screen.js
@@ -5,22 +5,23 @@ import Button from '../buttons/button'
 
 const SummaryScreen = () => {
     return (
-        <div className="d-flex jc-center">
+        <div>
             <div className="card d-flex">
-                <div className="card__date">
+                <div className="card__date d-flex flex-dir-column">
                     <h2>Александр Александрович</h2>
-                    <p className="card__mail">Andrii@mail.ua</p>
-                    <p className="card__location">Москва, Украина</p>
+                    <p className="card__mail ">Andrii@mail.ua</p>
+                    <p className="card__location ">Москва, Украина</p>
                     <p className="card__social">Facebook </p>
                 </div>
                 <div className="card__picture">
-                    <h2>ggg</h2>
+                    <h2>picture</h2>
                 </div>
             </div>
-            <Button className="d-flex jc-center" disabled = {false}>
-                Пройти заново           
-            </Button>
-            
+            <div className="d-flex jc-center">
+                <Button className="d-flex" disabled = {false}>
+                    Пройти заново           
+                </Button>
+            </div>
         </div>
     )
 } 
