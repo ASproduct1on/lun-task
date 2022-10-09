ec952ca18f1b1a7a00dc424cb1c5729284ffa481
Andrii Skega <Game.a.s.production@gmail.com>
2022-10-09 09:58:53 +0200

    feat: do everything

--- src/components/summary-screen/summary-screen.js
+++ src/components/summary-screen/summary-screen.js
@@ -1,29 +1,29 @@
 import React from "react";
-import './summary-screen.css'
-import Button from '../buttons/button'
-
+import "./summary-screen.css";
+import Button from "../button/button";
+import teamImg from "./assets/team.jpg";
 
 const SummaryScreen = () => {
-    return (
-        <div>
-            <div className="card d-flex">
-                <div className="card__date d-flex flex-dir-column">
-                    <h2>Александр Александрович</h2>
-                    <p className="card__mail ">Andrii@mail.ua</p>
-                    <p className="card__location ">Москва, Украина</p>
-                    <p className="card__social">Facebook </p>
-                </div>
-                <div className="card__picture">
-                    <h2>picture</h2>
-                </div>
-            </div>
-            <div className="d-flex jc-center">
-                <Button className="d-flex" disabled = {false}>
-                    Пройти заново           
-                </Button>
-            </div>
+  return (
+    <div>
+      <div className="card d-flex">
+        <div className="card__date d-flex flex-dir-column">
+          <h2>Александр Александрович</h2>
+          <p className="card__mail ">Andrii@mail.ua</p>
+          <p className="card__location ">Москва, Украина</p>
+          <p className="card__social">Facebook </p>
         </div>
-    )
-} 
+        <div className="card__picture">
+          <img src={teamImg} className="last-img" />
+        </div>
+      </div>
+      <div className="d-flex jc-center">
+        <Button className="d-flex" disabled={false}>
+          Пройти заново
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default SummaryScreen;
